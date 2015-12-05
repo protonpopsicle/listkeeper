93a3cf6596bc75074ff2f24626521fd63be3a434
Scott Meisburger <smeisburger@gmail.com>
2015-12-04 19:59:30 -0500

    someshit

--- listkeeper.sh
+++ listkeeper.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-awk 'BEGIN { RS = ""; FS = "\n"; fmt = "%-36.35s|:%-10.9s:\n" }; { printf fmt, $1, $2, $3 }' books.dat
+sed 's/^[[:blank:]]*//g' $1 | awk '
+    BEGIN {
+        RS = "";
+        FS = "\n";
+        format = "%-36.36s | %-20.20s | %-15.15s | %-10.10s\n"
+    };
+    NR == 1 {
+        printf format, toupper($1), toupper($2), toupper($3), toupper($4)
+        printf format, "---", "---", "---", "---"
+    };
+    NR > 1 {
+        printf format, $1, $2, $3, $4
+    };
+' | less -FX

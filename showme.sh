996c098fe309b3204825b2a5342fe093aec1dddc
Scott Meisburger <smeisburger@gmail.com>
2016-01-05 12:18:22 -0500

    yard

--- showme.sh
+++ showme.sh
@@ -3,16 +3,26 @@
 set -o errexit
 set -o nounset
 
-o1="****[ TODO ]****"
-o2=$(sh listkeeper.sh -f ~/Dropbox/todo.list -s priority)
-o3="****[ PROJECTS ]****"
-o4=$(sh listkeeper.sh -f ~/Dropbox/projects.list -s importance -r)
-output="$o1\n$o2\n\n$o3\n$o4"
+# WIDTH=$(tput cols)
 
-clear
+## Print horizontal ruler with message
+rulem ()  {
+    # Fill line with ruler character (default "-"), reset cursor, move 2 cols right, print message
+    printf -v _hr "%*s" $(tput cols) && echo -en ${_hr// /${2--}} && echo -e "\r\033[2C$1"
+}
 
-if [ -t 1 ]; then # stdout is a tty
-    printf "$output" | less --no-init --quit-if-one-screen --raw-control-chars
-else
-    printf "$output"
-fi
+tput clear
+# tput setb 4
+tput setaf 3
+tput bold
+rulem '[ TODO ]'
+tput sgr0
+tput setaf 4
+printf "$(sh listkeeper.sh -f ~/Dropbox/todo.list -s priority)\n"
+tput setaf 5
+tput bold
+rulem '[ PROJECTS ]'
+tput setaf 6
+tput sgr0
+printf "$(sh listkeeper.sh -f ~/Dropbox/projects.list -s importance -r)\n"
+printf "\n"
